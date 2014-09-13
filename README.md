e61d25375a43aa0eb316fb41faa3801679dd85a8
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-09-13 17:51:20 -0400

    Push the script to public Github repository

---
+++ README.md
@@ -0,0 +1,6 @@
+This script automates creating an empty Ruby on Rails application with postgresql and deploying it to Github.
+It assumes the User has installed postgresql and logged into Github on their machine, and created an empty 
+Github repository with the same name as the Rails application.
+
+Instructions for how to use the script can be obtained by running the script without command line parameters.
+
---
+++ newRailsApp.sh
@@ -0,0 +1,38 @@
+# Automates the steps for creating an empty Ruby on Rails application and deploying it to Github,
+# based on https://devcenter.heroku.com/articles/getting-started-with-ruby#set-up.
+# Assumes the User has installed and enabled postgresql on their machine.
+
+# Takes several command line parameters:
+# 	- The name of the app
+#	- The postgresql database username
+#	- The postgresql database password
+#	- Your Github username
+RUBY_VERSION=2.1.1
+
+if [ "$#" -lt 4 ]; then
+  echo "ERROR: Please enter 4 command line parameters:"
+  echo "- The name of the app"
+  echo "- The postgresql database username"
+  echo "- The postgresql database password"
+  echo "- Your Github username"
+  exit 1
+fi
+echo "Creating new Rails app "$1
+rails new $1 --database=postgresql
+cd $1/
+rails g controller welcome
+sed "s/# root 'welcome#index'/root 'welcome#index'/g" config/routes.rb > config/routes2.rb
+mv config/routes2.rb config/routes.rb
+printf "$( cat Gemfile )\n\ngem 'rails_12factor', group: :production\n\nruby \"$RUBY_VERSION\"\n" > Gemfile
+bundle install
+sed "s/username: $1/username: $2/g" config/database.yml > config/database2.yml
+sed "s/password:/password: $3/g" config/database2.yml > config/database.yml
+rm config/database2.yml
+
+bundle exec rake db:create
+
+git init
+git add .
+git commit -m "Initialized empty Rails application"
+git remote add origin git@github.com:/$4/$1.git
+git push -u origin master
